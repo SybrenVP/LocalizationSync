6911471c5129b0f261a0f71889a9c8e154817658
Sybren <sybren@rogueside.com>
2025-11-15 16:49:06 +0100

    Clean window and install button

--- addons/localization_sync/scripts/localization_sync_install_button.gd
+++
@@ -1,6 +0,0 @@
-extends Button
-
-const installUrl = "https://script.google.com/d/1IL41CvkjEm3Xn2hYrT98iDPzNASVngsRVCkVrISt7OjX-Os-2d-iQqiO/copy"
-
-func _on_pressed() -> void:
-	OS.shell_open(installUrl)
